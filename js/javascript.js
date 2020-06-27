3f6cc12fed55b7da91fcf5663e0671a81091d262
rachsg7 <sch19051@byui.edu>
2020-06-27 00:02:20 -0700

    First commit

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+const requestURL = 'pkmn.json';
+
+fetch(requestURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(jsonObject) {
+        console.table(jsonObject); // temporary cecking for valid response and data parsing
+        const types = jsonObject['types'];
+        for (let i = 0; i < types.length; i++) {
+            let card = document.createElement('div');
+            let span = document.createElement('span');
+
+            span.textContent = types[i];
+
+            card.appendChild(span);
+
+            document.querySelector('grid-container').appendChild(card);
+        }
+    })
